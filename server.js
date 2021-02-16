1690034729f9c005efba21cb557718a56fcdb230
Abigail Owen <abigai.h.owen.23@dartmouth.edu>
2021-02-16 16:44:50 +0000

    use request

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express().use(bodyParser.json());
 const fetch = require('node-fetch');
+const request = require('request');
 
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
@@ -78,15 +79,16 @@ function callSendAPI(sender_psid, response) {
     "message": response
   };
 
-  fetch(`https://graph.facebook.com/v2.6/me/messages`, {
-    method: 'POST',
-    headers: {
-      "Authoization": `Bearer ${process.env.PAGE_ACCESS_TOKEN}`
-    },
-    body: {
-      request_body
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
     }
-  }).then(data=>data.json()).then(function(response) {
-    console.log(response);
-  })
+  });
 }
