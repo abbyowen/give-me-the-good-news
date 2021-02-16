59942662a64ca7b1922c4baccf50cb440c06285a
Abigail Owen <abigai.h.owen.23@dartmouth.edu>
2021-02-16 19:59:48 +0000

    json()

--- server.js
+++ server.js
@@ -135,7 +135,7 @@ function handlePostback(sender_psid, received_postback) {
       "method": "GET"
     }, (err, res, body) => {
       if (!err) {
-        console.log(res);
+        console.log(res.json());
       } else {
         console.log('An error occured'+error);
       }
