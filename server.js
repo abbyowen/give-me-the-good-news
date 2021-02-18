d556cbc8797503a1d69a87bcd5e21761b843af5a
Abigail Owen <abigai.h.owen.23@dartmouth.edu>
2021-02-18 00:27:16 +0000

    debug

--- server.js
+++ server.js
@@ -318,7 +318,7 @@ function getVaccineNews(sender_psid) {
       for (var i=0; i<articles.length; i++) {
         var snippet = articles[i].snippet;
         var url = articles[i].web_url;
-        var title = articles[i].main;
+        var title = articles[i].abstract;
         console.log(`main title: ${title}`);
         console.log(snippet);
 
